80f1d564ffab9abe3586f28107d7f69ffa4bfe4b
karimsaidane007 <karim.saidane@gmail.com>
2026-01-12 22:21:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/promo-tunisie/',
+  base: '/promos-tunisie/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
